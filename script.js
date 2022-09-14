4dd35587f409c2b002e6aa4f8fca922fb46bea37
Mau <mauriciom.curiel@gmail.com>
2022-09-13 20:34:23 -0700

    adds letter sorting for letter bank

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ var letterBankElem = document.getElementById("letter-bank");
 function resetPage(){}
 
 function newName(){
-  resetPage();
-  var inputName = nameInputElem.value;
-  letterBankElem.innerText = getLetters(inputName);
+    resetPage();
+    var inputName = nameInputElem.value;
+    letterBankElem.innerText = getLetters(inputName);
 }
 
 function getLetters(input){
-  //clean text
-  input = input.toLowerCase();
-  input = input.replace(/[^a-zA-Z]*/g, "");//only keeps letters
-  console.log(input);
-  
-  //sort
-  var letters = input.split("");
+    //clean text
+    input = input.toLowerCase();
+    input = input.replace(/[^a-zA-Z]*/g, "");//only keeps letters
+    console.log(input);
 
-  return input;
+    //sort
+    var letters = input.split("");
+    letters.sort()
+    input = letters.join('');
+
+    return input;
 }
