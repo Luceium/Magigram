208a998742166926de3ade8806cff8b2d3db3d96
Mau <79354255+Luceium@users.noreply.github.com>
2023-04-25 20:32:20 -0700

    Creates a store with a letter frequency

---
+++ src/store.jsx
@@ -0,0 +1,18 @@
+import { createStore } from 'redux';
+
+const initialState = {
+letterFrequency: {}
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'UPDATE_DATA':
+      return { ...state, frequency: action.payload };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+export default store;
