b0f4d6e02f9073eeb4a8c3030bf2fce168545dfc
Mauricio Curiel <mauriciom.curiel@gmail.com>
2023-05-11 00:07:43 -0700

    creates a name generating algorithm that attempts to make a combination of roots and icons or prefixes I will likely replace this with an AI generated name, an API, or use a tool approach such as the one used for making the phrases

--- src/components/wordFinder.jsx
+++ src/components/wordFinder.jsx
@@ -8,6 +8,7 @@ export default function WordFinder() {
     const dispatch = useDispatch();
     let words = useSelector(state => state.words);
     let letterFrequency = useSelector(state => state.letterFrequency);
+    const lettersSize = getLetterFrequencySize(letterFrequency);
     const [word, setWord] = useState('');
     const [pos, setPos] = useState('noun'); // part of speech [noun, verb, adjective]
     const [filter, setFilter] = useState('Starts With'); // filter word choices by prefix [word, prefix
@@ -15,6 +16,14 @@ export default function WordFinder() {
     useEffect(() => {
         getWordChoices();
     }, [letterFrequency, word, pos])
+
+    function getLetterFrequencySize(letterFrequency) {
+        let size = 0;
+        for (const letter in letterFrequency) {
+            size += letterFrequency[letter];
+        }
+        return size;
+    }
     
     function handleChange(word) {
         setWord(word);
@@ -40,8 +49,6 @@ export default function WordFinder() {
         setFilter(filter);
     }
 
-    // TODO: use 'word' (value in input field) to further filter word choices
-    // ie. word.startsWith(word) or word.includes(word)
     // TODO: populate word choices in real time so user can see progress
     function getWordChoices() {
         let wordChoices = [];
@@ -67,6 +74,307 @@ export default function WordFinder() {
         setWordChoices(wordChoices);
     }
 
+    // takes the letter frequency and creates generates 20 random permutations of the letters such that the vowels are evenly distributed
+    function generateNames() {
+        if (4 < lettersSize && lettersSize < 10) {
+            alert('Please have 5 to 9 letters in your letter bank.');
+            return;
+        }
+
+        let names = [];
+        let vowels = ['a', 'e', 'i', 'o', 'u'];
+        let vowelFrequency = 0;
+        let consonantFrequency = 0;
+        for (const letter in letterFrequency) {
+            if (vowels.includes(letter)) {
+                vowelFrequency += letterFrequency[letter];
+            } else {
+                consonantFrequency += letterFrequency[letter];
+            }
+        }
+
+        // try to make better names by use of roots, prefix, sufix, common sounds
+        // TODO: sort the lists in a topological ordering such that if A.letterFrequency is a subset of B.letterFrequency, then B comes before A
+        // this would favor longer and rarer roots/prefixes/suffixes over shorter ones
+        // also randomize the order of the roots/prefixes/suffixes so that the same ones don't always appear first
+        const roots = [
+            'ab',
+            'abs',
+            'ad',
+            'ac',
+            'af',
+            'ag',
+            'al',
+            'an',
+            'ap',
+            'ar',
+            'as',
+            'at',
+            'auto',
+            'bi',
+            'cap',
+            'capt',
+            'ced',
+            'cess',
+            'chron',
+            'circum',
+            'clam',
+            'clamor',
+            'cogn',
+            'cognosc',
+            'corp',
+            'corpor',
+            'cred',
+            'credit',
+            'cycl',
+            'dem',
+            'demos',
+            'dict',
+            'duc',
+            'duct',
+            'equi',
+            'fac',
+            'fact',
+            'fer',
+            'port',
+            'fid',
+            'fide',
+            'fil',
+            'fili',
+            'flex',
+            'flect',
+            'fort',
+            'gen',
+            'gener',
+            'grad',
+            'gress',
+            'gravit',
+            'hab',
+            'habit',
+            'hydr',
+            'in',
+            'inter',
+            'jud',
+            'judici',
+            'lat',
+            'later',
+            'liber',
+            'loc',
+            'loc',
+            'luc',
+            'luc',
+            'man',
+            'manu',
+            'mar',
+            'marit',
+            'medi',
+            'morph',
+            'mut',
+            'nat',
+            'nat',
+            'nec',
+            'mort',
+            'nov',
+            'nov',
+            'omni',
+            'oper',
+            'pac',
+            'pac',
+            'path',
+            'path',
+            'phil',
+            'phon',
+            'phys',
+            'plan',
+            'plan',
+            'poss',
+            'pot',
+            'prim',
+            'pro',
+            'psych',
+            'radi',
+            'rupt',
+            'sci',
+            'scien',
+            'sect',
+            'sec',
+            'sem',
+            'semi',
+            'sent',
+            'sens',
+            'serv',
+            'sign',
+            'sign',
+            'sol',
+            'sol',
+            'spec',
+            'spect',
+            'spi',
+            'spir',
+            'stat',
+            'statu',
+            'ster',
+            'ster',
+            'sub',
+            'super',
+            'tac',
+            'tacit',
+            'temp',
+            'temp',
+            'ter',
+            'terr',
+            'therm',
+            'thes',
+            'thes',
+            'trans',
+            'tri',
+            'vid',
+            'vis',
+            'viv',
+            'vit',
+            'volut',
+        ].filter(word => isSubset(mapLetterFrequency(word), letterFrequency)).sort(()=>Math.random()-0.5);
+        const commonSounds = [
+            'ch',
+            'sh',
+            'th',
+            'ph',
+            'wh',
+            'wr',
+            'ck',
+            'gn',
+            'kn',
+            'ng',
+            'qu',
+            'sc',
+        ].filter(word => isSubset(mapLetterFrequency(word), letterFrequency));
+
+        // generate names by trying to use roots and suffixes and prefixes
+        let i = 0;
+        while (names.length < 20) {
+            // choose random root, prefix, and suffix and remove the letters from the letter frequency
+            let root = roots[i % roots.length];
+            letterFrequency = subtractLetterFrequency(letterFrequency, mapLetterFrequency(name));
+
+            const prefixes = [
+                'a',
+                'ab',
+                'ad',
+                'an',
+                'ante',
+                'ap',
+                'ar',
+                'as',
+                'at',
+                'auto',
+                'be',
+                'by',
+                'co',
+                'com',
+                'con',
+                'de',
+                'dis',
+                'e',
+                'en',
+                'ex',
+                'extra',
+                'ge',
+                'in',
+                'inter',
+                'ir',
+                'mis',
+                'non',
+                'per',
+                'pre',
+                'pro',
+                're',
+                'semi',
+                'sub',
+                'super',
+                'un',
+                'under',
+                'up',
+                'with',
+                'y'
+              ].filter(word => isSubset(mapLetterFrequency(word), letterFrequency));
+            const prefix = prefixes.length > 0 ? prefixes[Math.floor(Math.random()*prefixes.length)] : '';
+            letterFrequency = subtractLetterFrequency(letterFrequency, mapLetterFrequency(prefix));
+            
+            const suffixes = [
+                'able',
+                'age',
+                'al',
+                'an',
+                'and',
+                'ary',
+                'ate',
+                'ation',
+                'ative',
+                'ed',
+                'ee',
+                'er',
+                'es',
+                'et',
+                'ful',
+                'ible',
+                'ion',
+                'ish',
+                'ist',
+                'ity',
+                'ize',
+                'less',
+                'ly',
+                'ment',
+                'ness',
+                'on',
+                'or',
+                'ous',
+                's',
+                'th',
+                'tion',
+                'tive',
+                'y'
+            ].filter(word => isSubset(mapLetterFrequency(word), letterFrequency));
+            const suffix = suffix.length > 0 ? suffixes[Math.floor(Math.random()*suffixes.length)] : ''; 
+            letterFrequency = subtractLetterFrequency(letterFrequency, mapLetterFrequency(suffix));
+            
+            // generate name by stringing together root, prefix, and suffix and padding with the remaining letters from the letter frequency
+            let left, right = makePadding(letterFrequency);
+            let name = prefix + leftPad + root + rightPad + suffix;
+            
+            names.add(name)
+            i++;
+        }
+
+        // generate remaining amount of names with equal distribution of vowels between consonants
+        for (let i = names.length; i < 20; i++) {
+            let name = '';
+
+            names.push(name);
+        }
+        setWordChoices(names);
+    }
+
+    // evenly splits the remaining letters in the letter frequency between the left and right padding and randomly organizes the letters in each
+    function makePadding(letterFrequency) {
+        let leftPad = '';
+        let rightPad = '';
+        let left = true;
+        let lettersSize = getLetterFrequencySize(letterFrequency);
+        while (lettersSize > 0) {
+            let letter = letterFrequency[Math.floor(Math.random()*letterFrequency.length)];
+            letterFrequency = letterFrequency.removeLetters(letter);
+            if (left) {
+                leftPad += letter;
+            } else {
+                rightPad += letter;
+            }
+            left = !left;
+            lettersSize--;
+        }
+        return [leftPad, rightPad];
+    }
+    
     return (
         <>
             <h1>Word Finder</h1>
@@ -107,6 +415,7 @@ export default function WordFinder() {
                         Adjective
                     </Button>
                 </ButtonGroup>
+                <Button onClick={generateNames}>Generate Names</Button>
             </div>
             <WordGroup src={wordChoices} type='wordList' />
         </>
