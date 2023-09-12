79d7fb019a2c6a43edf346b19899ff2d8f7bc345
Mau <79354255+Luceium@users.noreply.github.com>
2023-09-12 00:08:24 -0700

    attempts to implement drag and drop

--- src/components/wordGroup.jsx
+++ src/components/wordGroup.jsx
@@ -2,21 +2,45 @@ import React from 'react'
 import Word from './word';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useRef } from 'react';
 
 export default function wordGroup(props) {
     const words = props.src;
     const [draggedIndex, setDraggedIndex] = useState(-1);
     const dispatch = useDispatch();
-    // const 
+    const containerRef = useRef(null);
 
     function handleDragOver(e) {
+        //TODO: handle instent send to back
+        //TODO: handle constantly swapping words when on border (bc the swap causes the center to change which triggers another swap). The solution is too
         e.preventDefault();
         let wordRemoved = words.splice(draggedIndex, 1)[0]
 
-        //TODO: add logic to determine where to insert the dragged word
-        const insertIndex = 0;
-        words.splice(insertIndex, 0, wordRemoved)
-        setDraggedIndex(insertIndex)
+        const children = [...containerRef.current.children].slice(draggedIndex)
+
+        const insertIndex = children.reduce(
+            (closest, child, index, arr) => {
+                const box = child.getBoundingClientRect()
+                const offset = e.clientX - box.left - box.width
+                if (offset < 0 && offset > closest.offset) {
+                    return { offset: offset, insertIndex: index }
+                } else {
+                    return closest
+                }
+            },
+            {offset: Number.NEGATIVE_INFINITY}
+        ).insertIndex;
+        console.log(insertIndex)
+
+        if (insertIndex) {        
+            console.log(words)
+            words.splice(insertIndex, 0, wordRemoved)
+            console.log(words)
+            setDraggedIndex(insertIndex)
+        } else {
+            words.push(wordRemoved)
+            setDraggedIndex(words.length-1)
+        }
     }
 
     function handleDragEnd() {
@@ -28,7 +52,7 @@ export default function wordGroup(props) {
 
 
     return (
-        <div onDragOver={(e) => handleDragOver(e)}>
+        <div onDragOver={(e) => handleDragOver(e)} ref={containerRef} >
             {words.map((word, index) => {
                 return (
                     <Word
