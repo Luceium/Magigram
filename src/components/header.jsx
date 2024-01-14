d8ee2bf92ea7481f54785ec30c7340579e835a17
Mau <79354255+Luceium@users.noreply.github.com>
2024-01-14 13:07:12 -0800

    adds an indicator on the info button until the user clicks it for the first time

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Instructions from './Instructions';
 
 function Header(){
+    // local storage only stores strings and with 2 bytes per character so t is used to represent true.
+    const [isInfoNeverClicked, infoClicked] = useState(localStorage.getItem('isInfoNeverClicked') == null);
+
     return (
-        <div className='navbar bg-base-100'>
-            <div className="flex-1">
+        <div className='navbar bg-primary'>
+            <div className="flex-1 text-primary-content">
                 <h1 className="text-3xl">Magigram</h1>
             </div>
+
+            <div className="indicator">
+                {isInfoNeverClicked && <span className="indicator-item badge badge-primary">new</span>}
+                {/* Open the modal using document.getElementById('ID').showModal() method */}
+                <button className="btn text-info m-2" onClick={()=>document.getElementById('tip_modal').showModal()}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-8 h-8"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                </button>
+                <dialog id="tip_modal" className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                
+                    <Instructions />
+                    <div className="modal-action">
+                    <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn" onClick={() => {infoClicked(false); localStorage.setItem("isInfoNeverClicked", "f"); console.log(`modal closed. state: ${isInfoNeverClicked}. storage: ${localStorage.getItem("isInfoNeverClicked")}`)}}>Close</button>
+                    </form>
+                    </div>
+                </div>
+                </dialog>
+            </div>
+
             <select data-choose-theme class="select select-primary max-w-xs">
             {[
                         "cyberpunk",
