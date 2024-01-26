5667f96c975d0724e93752487eb40200503bf970
Mau <79354255+Luceium@users.noreply.github.com>
2024-01-25 17:15:42 -0800

    ensures visibility of tabs on all themes

--- src/components/nameBuilder.jsx
+++ src/components/nameBuilder.jsx
@@ -7,18 +7,14 @@ const NameBuilder = () => {
     const [isAnagramBuilder, switchBuilder] = useState(true)
 
   return (
-    <div class="mt-3 p-2 flex-1 bg-neutral text-neutral-content rounded">
+    <div class="mt-3 p-2 flex-1 bg-neutral text-neutral-content rounded flex flex-col gap-2">
         <div role="tablist" className="tabs tabs-lifted tabs-lg">
-            <button role="tab" className={`tab ${isAnagramBuilder ? "tab-active" : ""}`} onClick={() => switchBuilder(!isAnagramBuilder)}>Word Finder</button>
-            <button role="tab" className={`tab ${!isAnagramBuilder ? "tab-active" : ""}`} onClick={() => switchBuilder(!isAnagramBuilder)}>Name Builder</button>
+            <button role="tab" className={`tab ${isAnagramBuilder ? "tab-active" : "text-neutral-content"}`} onClick={() => switchBuilder(true)}>Word Finder</button>
+            <button role="tab" className={`tab ${!isAnagramBuilder ? "tab-active" : "text-neutral-content"}`} onClick={() => switchBuilder(false)}>Name Builder</button>
         </div>
-        <div className='mx-auto'>
-            <div className=' bg-neutral rounded mt-3' >
-                {isAnagramBuilder ?
-                <WordFinder types={['noun', 'verb', 'adjective']} word /> :
-                <WordFinder types={['prefix', 'root', 'suffix']}/>}
-            </div>
-        </div>
+        {isAnagramBuilder ?
+        <WordFinder types={['noun', 'verb', 'adjective']} word /> :
+        <WordFinder types={['prefix', 'root', 'suffix']}/>}
         <div className=' bg-neutral rounded' >
             <h2> Word Bank </h2>
             <WordGroup type='wordBank' src={useSelector(state => state.words)}/>
