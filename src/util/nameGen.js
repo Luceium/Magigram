a733193574575e8f64d0551c41a2dd75d4e79b3a
Mau <79354255+Luceium@users.noreply.github.com>
2024-02-16 01:26:15 -0800

    debugging

--- src/util/nameGen.js
+++ src/util/nameGen.js
@@ -1,22 +1,62 @@
-import { cleanFrequency, getLetterFrequencySize } from "./frequencyUtils";
+import { cleanFrequency, getLetterFrequencySize, removeLetter } from "./frequencyUtils";
+
+function getModel() {
+    // TODO: get All models and user weights from store to generate model
+    const model = require(`../nameModel/models/masterModel.json`);
+    console.log(model, model['a']);
+    return model;
+}
 
 // removes letters which cannot be used in the name from the letter frequency
 function validateModel(model, letterFrequency) {
+    console.log(model);
     // remove letters from model that are not in the letter frequency
-    let validModel = { ...model }
-    for (const letter in model) {
-        if (!letterFrequency[letter] || letterFrequency[letter] < 1) {
-            delete validModel[letter];
+    let validModel = {}
+    console.log(validModel);
+    for (const letter in letterFrequency) {
+        if (letterFrequency[letter] > 0) {
+            validModel[letter] = model[letter];
+            console.log(validModel);
         }
     }
+    console.log(validModel);
     
-    return validateModel
+    return validModel;
+}
+
+export function generateTransformerPoweredNames(letterFrequency) {
+    const lettersForName = cleanFrequency({ ...letterFrequency });
+    const model = validateModel(getModel(), lettersForName)
+    console.log(model, model['a']);
+    let names = [];
+    for (let i = 0; i < 20; i++) {
+        names.push(generateName(model, lettersForName));
+    }
+    return names;
+}
+
+function generateName(model, lettersForName) {
+    lettersForName = { ...lettersForName };
+    let letter;
+    // Selects random first letter from lettersForName
+    const keys = Object.keys(lettersForName);
+    let name = keys[Math.floor(Math.random()*keys.length)];
+    lettersForName = removeLetter(lettersForName, name); // name at this point is 1 character long
+    
+    for (let i = 0; i < getLetterFrequencySize(lettersForName); i++) {
+        console.log(name, model, model[name[i]])
+        letter, lettersForName = selectNextLetter(0.5, lettersForName, model[name[i]]);
+        console.log(lettersForName);
+        name += letter;
+    }
+    return name;
 }
 
 function selectNextLetter(temperature, lettersForName, nextLetterProbability) {
     let chosenLetter;
 
     // create percentages for each letter adding up to 1
+    console.log(nextLetterProbability);
     let sum = 0;
     for (const letter in nextLetterProbability) {
         sum += nextLetterProbability[letter];
@@ -24,7 +64,8 @@ function selectNextLetter(temperature, lettersForName, nextLetterProbability) {
     for (const letter in nextLetterProbability) {
         nextLetterProbability[letter] = nextLetterProbability[letter] / sum;
     }
-
+    console.log(nextLetterProbability);
+    
     // sort valid letters by probability
     let validLetters = [];
     for (const letter in lettersForName) {
@@ -36,6 +77,7 @@ function selectNextLetter(temperature, lettersForName, nextLetterProbability) {
     // works by selecting a random number and checking if it is in the range of the probability of the letter
     // the temperature is used to limit the range of the probability to the top x% of the probability
     let random = 1 - Math.random() * temperature;
+    // console.log(random,validLetters, nextLetterProbability);
     sum = 1;
     for (const letter in validLetters) {
         sum -= nextLetterProbability[letter];
@@ -46,35 +88,9 @@ function selectNextLetter(temperature, lettersForName, nextLetterProbability) {
     }
 
     // remove letter from letters left for name
+    // console.log(chosenLetter);
     lettersForName = removeLetter(lettersForName, chosenLetter);
+    // console.log(lettersForName);
     // return letter and new lettersForName
-    return [letter, lettersForName];
-}
-
-function getModel() {
-    // TODO: get All models and user weights from store to generate model
-    return require(`../nameModel/models/masterModel.json`)
-}
-
-export function generateTransformerPoweredNames() {
-    let lettersForName = cleanFrequency({ ...letterFrequency });
-    model = validateModel(getModel(), lettersForName)
-    for (let i = 0; i < 20; i++) {
-        names.push(generateName(model, lettersForName));
-    }
-}
-
-function generateName(model, lettersForName) {
-    let lettersForName = { ...lettersForName };
-    let letter;
-    // Selects random first letter from lettersForName
-    const keys = Object.keys(lettersForName);
-    let name = keys[Math.floor(Math.random()*keys.length)];
-    lettersForName = removeLetter(lettersForName, name); // name at this point is 1 character long
-
-    for (let i = 0; i < getLetterFrequencySize(lettersForName); i++) {
-        letter, lettersForName = selectNextLetter(0.5, lettersForName, model[name[i]]);
-        name += letter;
-    }
-    return name;
+    return [chosenLetter, lettersForName];
 }
