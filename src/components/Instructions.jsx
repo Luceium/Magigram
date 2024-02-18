8414dfbea4313d55f6ddfe4079f29f0b54baa9f8
Mau <79354255+Luceium@users.noreply.github.com>
2024-02-17 23:07:58 -0800

    style instructions

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,16 +1,24 @@
 export default function Instructions() {
     return (
         <>
-            <h1>Info Center</h1>
-            <p>Goal: </p>
-            <p></p>
-            <p>Instructions: </p>
+            <h1 className=" text-3xl">Strategy</h1>
+            <h2 className="text-xl">Instructions: </h2>
             <ol>
-                <li></li>
+                <li className="ml-7 list-decimal">Input your full name in the name section.</li>
+                <li className="ml-7 list-decimal">Start by finding all the cool or interesting words you might want in your username.</li>
+                <li className="ml-7 list-decimal">Click on the save button to keep those words around.</li>
+                <li className="ml-7 list-decimal">Now try clicking on a save, and build out your username by selecting more words.</li>
+                <li className="ml-7 list-decimal">Save as you go, when you want to switch to a different save, or when you hit a fork in the road!</li>
+                <li className="ml-7 list-decimal">When you're ready, go to the Name Builder tab and generate a name. Look at the roots, prefixes, and suffixes to try to build your own name.</li>
+                <li className="ml-7 list-decimal">Now generate an animation for your name and share!</li>
+                <li className="ml-7 list-decimal">Don't forget to use your awesome new username and donate if you liked this app :)</li>
             </ol>
-            <p>Tips: </p>
+            <h2 className="text-xl">Tips: </h2>
             <ul>
-                <li></li>
+                <li className="ml-7 list-disc">Use the "Starts With" filter to search for a word or variant of a word you have in mind.</li>
+                <li className="ml-7 list-disc">Use the "Contains" filter to find words that use difficult characters or abundant characters. You wouldn't want to generate a name with 5 A's in it.</li>
+                <li className="ml-7 list-disc">Search for a noun and adjective before generating a name.</li>
+                <li className="ml-7 list-disc">A common start is "I am".</li>
             </ul>
         </>
     )
