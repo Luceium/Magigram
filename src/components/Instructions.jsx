536179c0583352217db23e947734ab94796f66c5
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-02-26 18:28:32 -0800

    update instructions

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -5,8 +5,8 @@ export default function Instructions() {
             <h2 className="text-xl">Instructions: </h2>
             <ol>
                 <li className="ml-7 list-decimal">Input your full name in the name section.</li>
-                <li className="ml-7 list-decimal">Start by finding all the cool or interesting words you might want in your username.</li>
-                <li className="ml-7 list-decimal">Click on the save button to keep those words around.</li>
+                <li className="ml-7 list-decimal">Get started by finding all the cool or interesting words you might want in your username, in the Word Finder.</li>
+                <li className="ml-7 list-decimal">Click on the save button in History to keep those words around as an attempt.</li>
                 <li className="ml-7 list-decimal">Now try clicking on a save, and build out your username by selecting more words.</li>
                 <li className="ml-7 list-decimal">Save as you go, when you want to switch to a different save, or when you hit a fork in the road!</li>
                 <li className="ml-7 list-decimal">When you're ready, go to the Name Builder tab and generate a name. Look at the roots, prefixes, and suffixes to try to build your own name.</li>
