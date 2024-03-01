a1e7e0161e8ecf13788909dfb29fd04432d86ba6
Mau <79354255+Luceium@users.noreply.github.com>
2024-03-01 14:48:38 -0800

    adds skyrim names to fantasyCorpus

--- nameModel/corpus/tmp.py
+++
@@ -1,9 +0,0 @@
-# remove duplicate entries in tmp.txt
-line = []
-with open('tmp.txt', 'r') as f:
-    lines = f.readlines()
-    lines = list(set(lines))
-
-with open('tmp.txt', 'w') as f:
-    for line in lines:
-        f.write(line)
